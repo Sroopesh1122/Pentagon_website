a2702a46866b678865de58ff90a23445b3745944
Sroopesh1122 <shivuroopesh6362@gmail.com>
2025-05-12 10:22:00 +0530

    changes by shivu

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
       <div className="w-screen h-screen flex items-center justify-center">
           
           <div >
-               <img src="https://i.pinimg.com/564x/0d/a8/9c/0da89cd80d1d48baedb06242d9b5c60d.jpg" className='w-[250px] h-auto scale-75 animate-slow-bounce' alt="" />
+               <img src="https://i.pinimg.com/564x/0d/a8/9c/0da89cd80d1d48baedb06242d9b5c60d.jpg" className='w-[250px] h-auto scale-50 animate-slow-bounce' alt="" />
            <h1 className='w-full text-center font-semibold flex justify-center items-center gap-2'>Entering Orbit... Hold On <LuLoaderCircle size={20} className='animate-spin duration-1000'/> </h1>
           
           </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,93 +1,91 @@
-import React, { useState } from 'react'
-import Brand from './Brand'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import Brand from "./Brand";
+import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
 
-const menuItems =[
+const menuItems = [
   {
-    key:"courses",
-    label:"COURSES",
-    path :".courses-section",
-  },{
-      key:"aboutUs",
-      label:"ABOUT US",
-      path:".about-section",
-  },{
-      key:"verifyCertificate",
-      label:"Verify Certificate",
-      path:"/verifyCertificate",
-  }
-] 
-
+    key: "courses",
+    label: "COURSES",
+    isLink: false,
+    section: ".courses-section",
+  },
+  {
+    key: "aboutUs",
+    label: "ABOUT US",
+    path: ".about-section",
+  },
+  {
+    key: "verifyCertificate",
+    label: "Verify Certificate",
+    path: "/verifyCertificate",
+  },
+];
 
 const Navbar = () => {
-
-
-  const [navbarOpen ,setNavbarOpen] = useState(false)
-
- 
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
     <>
-     <section className='w-full flex justify-between items-center p-3'>
-       <Brand/>
-         
+      <section className="w-full flex justify-between items-center p-3">
+        <Brand />
+        <FaBars
+          size={25}
+          className="flex lg:hidden cursor-pointer"
+          onClick={() => setNavbarOpen(true)}
+        />
+        <article className="hidden lg:flex justify-center items-center gap-5 uppercase font-[500] text-[0.9rem]">
+          {menuItems.map((item, idx) => (
+            <Link className="underline-nav" key={item.key} to={item.path}>
+              {item.label}
+            </Link>
+          ))}
+          <button>DOWNLOAD APP</button>
+          <button>LOGIN</button>
+        </article>
+      </section>
 
-       <FaBars size={25} className='flex lg:hidden cursor-pointer' onClick={()=>setNavbarOpen(true)}/> 
+      <section
+        className={`w-full fixed top-0 h-screen flex justify-end items-start bg-black/40 z-[1000] duration-400 ${
+          navbarOpen ? "right-0" : "-right-full"
+        }`}
+        onClick={() => setNavbarOpen(false)}
+      >
+        <article
+          className="w-fit h-full relative p-4 md:p-8 bg-white"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <span
+            className="absolute top-2 right-2 cursor-pointer"
+            onClick={() => setNavbarOpen(false)}
+          >
+            <IoMdClose size={25} />
+          </span>
 
+          <div className="w-full flex justify-start items-start flex-col gap-3">
+            {menuItems.map((item, idx) => (
+              <Link
+                className="underline-nav py-2 px-4 shadow rounded-2xl"
+                key={item.key}
+                to={item.path}
+              >
+                {item.label}
+              </Link>
+            ))}
 
-       <article className='hidden lg:flex justify-center items-center gap-5 uppercase font-[500] text-[0.9rem]'>
+            <button className="py-2 px-4 shadow rounded-2xl">
+              DOWNLOAD APP
+            </button>
 
-         {
-            menuItems.map((item,idx)=> <Link className='underline-nav' key={item.key}  to={item.path}>{item.label}</Link> )
-         }
-
-         <button >DOWNLOAD APP</button>
-
-         <button>LOGIN</button>
-
-       </article>
-
-        
-    </section>
-
-    <section 
-    className={`w-full fixed top-0 h-screen flex justify-end items-start bg-black/40 z-[1000] duration-400 ${navbarOpen ? "right-0" : "-right-full"}`}
-    onClick={()=>setNavbarOpen(false)}>
-
-      <article className='w-fit h-full relative p-4 md:p-8 bg-white' onClick={(e)=>e.stopPropagation()}> 
-         <span className='absolute top-2 right-2 cursor-pointer' onClick={()=>setNavbarOpen(false)}><IoMdClose size={25}/></span>
-         
-         <div className='w-full flex justify-start items-start flex-col gap-3'>
-
-          {
-            menuItems.map((item,idx)=> <Link className='underline-nav py-2 px-4 shadow rounded-2xl' key={item.key}  to={item.path}>{item.label}</Link> )
-          }
-
-         <button className='py-2 px-4 shadow rounded-2xl' >DOWNLOAD APP</button>
-
-         <button className='py-2 px-4 shadow rounded-2xl'>LOGIN</button>
-
-         </div>
-
-
-      
-      </article> 
-
-    </section>
+            <button className="py-2 px-4 shadow rounded-2xl">LOGIN</button>
+          </div>
+        </article>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
 
-
-
-const MobileNavbar =()=>{
-
-}
-
-
-
-
+const MobileNavbar = () => {};
